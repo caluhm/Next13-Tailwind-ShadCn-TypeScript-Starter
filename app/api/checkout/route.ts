5bace4d4968546a731ac73ae6445005efa5f7f26
caluhm <calum.jarvo@gmail.com>
2023-09-22 22:46:40 +0100

    if this doesnt work its bedtime

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,32 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(String(process.env.STRIPE_SECRET_KEY), {
   apiVersion: "2023-08-16",
 });
 
-export default async function POST(req: any, res: any) {
-  if (req.method === "POST") {
-    try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            // TODO: replace this with the `price` of the product you want to sell
-            price: "price_1NtFsqFVRAT9rxdZhGOJUqdI",
-            quantity: 1,
-          },
-        ],
-        payment_method_types: ["card"],
-        mode: "payment",
-        success_url: `${req.headers.origin}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
-      res.redirect(303, session.url);
-    } catch (err: any) {
-      res.status(err.statusCode || 500).json(err.message);
-    }
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+export default async function POST(request: NextRequest) {
+  try {
+    // Create Checkout Sessions from body params.
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // TODO: replace this with the `price` of the product you want to sell
+          price: "price_1NtFsqFVRAT9rxdZhGOJUqdI",
+          quantity: 1,
+        },
+      ],
+      payment_method_types: ["card"],
+      mode: "payment",
+      success_url: `${request.headers.get(
+        "origin",
+      )}/?success=true&session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${request.headers.get("origin")}/?canceled=true`,
+    });
+    return NextResponse.redirect(String(session.url), { status: 303 });
+  } catch (err: any) {
+    return NextResponse.redirect(`/?error=${err.message}`, { status: 303 });
   }
 }
