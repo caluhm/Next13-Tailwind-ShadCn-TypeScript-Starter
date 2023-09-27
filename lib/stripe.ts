bb67ef42b8c070d9eeca287ff1a8c1e4e9955cec
caluhm <calum.jarvo@gmail.com>
2023-09-27 15:48:20 +0100

    success and cancel url was incorrect

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -6,8 +6,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 
 export async function createCheckoutLink(customer: string) {
   const checkout = await stripe.checkout.sessions.create({
-    success_url: "http://localhost:3000?success=true",
-    cancel_url: "http://localhost:3000?cancelled=true",
+    success_url: `https://${process.env.VERCEL_URL}?success=true`,
+    cancel_url: `https://${process.env.VERCEL_URL}?success=false`,
     customer: customer,
     line_items: [
       {
