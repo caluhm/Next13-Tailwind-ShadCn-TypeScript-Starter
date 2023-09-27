f0d77d820af23207893cd786096d3ce787a1bcd1
caluhm <calum.jarvo@gmail.com>
2023-09-27 13:40:46 +0100

    base path add

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -3,7 +3,7 @@
 import { SessionProvider } from "next-auth/react";
 
 const Provider = ({ children }: any) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider basePath="/api/auth">{children}</SessionProvider>;
 };
 
 export default Provider;
